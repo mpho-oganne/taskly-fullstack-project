65ac42fc6ba924d05aeae114a3e03ac75a9a48ae
Pumlani Kewana <pumlanibasil15@gmail.com>
2024-09-19 12:57:37 +0200

    resolving conflicts

--- server/server/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const auth = (req, res, next) => {
-    if (!req.session.userId) {
-      return res.status(401).json({ error: 'Unauthorized. Please log in.' });
-    }
-    next();
-  };
-
-  module.exports = auth;
