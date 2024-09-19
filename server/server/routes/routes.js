d39525aa810cd95cb977da8b85cedd016aaf3611
Konanani Nemauluma <koniecharity@gmail.com>
2024-09-19 11:12:49 +0200

    added routes for signin and signout

--- server/server/routes/routes.js
+++ server/server/routes/routes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const { validateSignUp } = require('../middleware/auth');
 
-const { signup } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 
 // Signup route
 router.post('/signup', validateSignUp, signup);
+router.post('/signin', signin);
+router.post('/signout', signout);
+
 
 module.exports = router;
