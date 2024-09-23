07dc637cda0bd4c193d799d162523eb12f1feed8
Konanani Nemauluma <koniecharity@gmail.com>
2024-09-23 13:01:44 +0200

    updated endpoints

--- server/server/routes/routes.js
+++ server/server/routes/routes.js
@@ -16,10 +16,13 @@ const {
 router.post('/signup', validateSignUp, signup);
 router.post('/signin', signin);
 router.post('/signout', signout);
+router.get('/', getUser)
 router.post('/create', createTask);
 router.put('/update/:id', updateTask);
+router.get('/:id')
 router.get('/', getTasks);
 router.delete('/delete/:id', deleteTask)
 router.post('/setReminder', setReminder); 
+
 
 module.exports = router;
