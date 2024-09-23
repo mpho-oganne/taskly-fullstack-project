90aff1414ee8a96711424c9b57d1b2ff06a3a29f
Konanani Nemauluma <koniecharity@gmail.com>
2024-09-23 12:40:17 +0200

    added routes for all the task functions

--- server/server/routes/routes.js
+++ server/server/routes/routes.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const router = express.Router();
 
 const { validateSignUp } = require('../validation/signup');
 
 const { signup, signin, signout } = require('../controllers/user');
+const router = express.Router();
+const {
+    createTask,
+    updateTask,
+    getTasks,
+    deleteTask,
+    setReminder
+  } = require('../controllers/task');
 
 // Signup route
 router.post('/signup', validateSignUp, signup);
 router.post('/signin', signin);
 router.post('/signout', signout);
-
+router.post('/create', createTask);
+router.put('/update/:id', updateTask);
+router.get('/', getTasks);
+router.delete('/delete/:id', deleteTask)
+router.post('/setReminder', setReminder); 
 
 module.exports = router;
