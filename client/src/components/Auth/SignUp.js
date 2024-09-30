64489d43f5bf4f8b3fc6cd4188ee96f3c709c53c
Konanani Nemauluma <koniecharity@gmail.com>
2024-09-30 12:42:36 +0200

    added starter code for signin and signup function

---
+++ client/src/components/Auth/SignUp.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const SignUp = () => {
+  return (
+    
+  );
+};
+
+export default SignUp;
