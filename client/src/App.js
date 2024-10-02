2ee206250bbf4a3fd5a721fbfecad6a83bf45381
NonhlanhlaMazibuko <nonhlanhlamazibuko17@gmail.com>
2024-10-02 23:24:20 +0200

    Worked on the structure of the Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage/HomePage';
-import SignIn from './components/Auth/SignIn'
-import SignUp from './components/Auth/SignUp'
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
+import Navbar from './components/Pages/Navbar/navbar';
 
 const App = () => {
   return (
 
     <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-    </Routes>   
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>   
   </Router>
     
   );
