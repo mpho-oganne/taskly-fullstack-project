abdc5505876f0a565b4b0df9109272caf516ef86
Konanani Nemauluma <koniecharity@gmail.com>
2024-10-03 11:08:47 +0200

    updated homepage route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/Pages/HomePage/HomePage';
+import HomePage from './Pages/HomePage/HomePage';
 import SignIn from './components/Auth/SignIn'
 import SignUp from './components/Auth/SignUp'
 
