8ecb85e1e07e8b9097bf0a5ff4c92eb4be1e6c6c
Konanani Nemauluma <98395053+Mukoni-Nemauluma@users.noreply.github.com>
2024-10-07 13:12:46 +0200

    Update updatetaskpage.js

--- client/src/Pages/Task/updatetaskpage.js
+++ client/src/Pages/Task/updatetaskpage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import UpdateTaskForm from './updateTask';  // Import the form component
 //import { useParams } from 'react-router-dom';  // To get the taskId from the URL
-
+// code looks good
 export default function UpdateTaskPage() {
+
+  //hard coded taskId needs to be retrieved from params
   const taskId  = '67028cbc3be34cb4fbc51ed5';  // Get the taskId from the URL (if using react-router)
 
   return (
