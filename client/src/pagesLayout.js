9e0cc6e98040a082f3b54b90aff7b60c3ca789db
NonhlanhlaMazibuko <nonhlanhlamazibuko17@gmail.com>
2024-10-08 16:23:54 +0200

    Resolving conflicts

---
+++ client/src/pagesLayout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/sidebar';
+import Profile from './Pages/Profile/profile';  
+
+function Layout() {
+  return (
+    <div className="flex h-screen">
+      {/* Navbar Section */}
+      <Sidebar />
+
+      {/* pages */}
+      <div className="flex-grow">
+        <Profile />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
+
