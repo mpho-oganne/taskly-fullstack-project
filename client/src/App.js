0db864c17f4ea1bbc991da4f5560c9b7a828bf3b
Konanani Nemauluma <koniecharity@gmail.com>
2024-10-14 20:36:50 +0200

    updated App routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,15 @@ import Layout from "./Layout/dashboardLayout";
 import SignIn from "./components/Auth/SignIn";
 import SignUp from "./components/Auth/SignUp";
 import UpdateTaskForm from "./Pages/Task/updateTask";
-import ManageTasks from './Pages/Task/manageTasks';
-import CreateTaskForm from './Pages/Task/createTask';
+import ManageTasks from "./Pages/Task/manageTasks";
+import CreateTaskForm from "./Pages/Task/createTask";
 import Calendar from "./components/Calender/calender";
 import Overview from "./components/Overview/overview";
 import HomepageLayout from "./Layout/HomePageLayout";
 import Leaderboard from "./components/Leaderboard/leaderboard";
 
 // Context
-import UserProvider from "./UserContext"; 
+import UserProvider from "./UserContext";
 import RequireAuth from "./requireAuth";
 
 const App = () => {
@@ -25,52 +25,97 @@ const App = () => {
       <Router>
         <Routes>
           {/* Public pages */}
-          <Route path="/" element={<HomepageLayout><HomePage /></HomepageLayout>} />
-          <Route path="/signup" element={<HomepageLayout><SignUp /></HomepageLayout>} />
-          <Route path="/signin" element={<HomepageLayout><SignIn /></HomepageLayout>} />
+          <Route
+            path="/"
+            element={
+              <HomepageLayout>
+                <HomePage />
+              </HomepageLayout>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <HomepageLayout>
+                <SignUp />
+              </HomepageLayout>
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <HomepageLayout>
+                <SignIn />
+              </HomepageLayout>
+            }
+          />
 
           {/* Protected routes */}
-          <Route path="/new-task" element={
-            <RequireAuth>
-              <CreateTaskForm />
-            </RequireAuth>
-          } />
-          <Route path="/dashboard" element={
-            <RequireAuth>
-              <Layout />
-            </RequireAuth>
-          } />
-          <Route path="/tasks" element={
-            <RequireAuth>
-              <ManageTasks />
-            </RequireAuth>
-          } />
-          <Route path="/update-task/:taskId" element={
-            <RequireAuth>
-              <UpdateTaskForm />
-            </RequireAuth>
-          } />
-          <Route path="/profile" element={
-            <RequireAuth>
-              <Profile />
-            </RequireAuth>
-          } />
-          <Route path="/calendar" element={
-            <RequireAuth>
-              <Calendar />
-            </RequireAuth>
-          } />
-          <Route path="/overview" element={
-            <RequireAuth>
-              <Overview />
-            </RequireAuth>
-          } />
-          <Route path="/leaderboard" element={
-            <RequireAuth>
-              <Leaderboard />
-            </RequireAuth>
-          } />
-          
+          <Route
+            path="/new-task"
+            element={
+              <RequireAuth>
+                <CreateTaskForm />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <Layout />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/tasks"
+            element={
+              <RequireAuth>
+                <ManageTasks />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/update-task/:taskId"
+            element={
+              <RequireAuth>
+                <UpdateTaskForm />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <RequireAuth>
+                <Profile />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <RequireAuth>
+                <Calendar />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/overview"
+            element={
+              <RequireAuth>
+                <Overview />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/leaderboard"
+            element={
+              <RequireAuth>
+                <Leaderboard />
+              </RequireAuth>
+            }
+          />
+
           {/* Fallback for unmatched routes */}
           <Route path="*" element={<h1>404 - Not Found</h1>} />
         </Routes>
