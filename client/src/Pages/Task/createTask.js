f2de4b7a5bbd5add59248aa25f184716152b97df
Konanani Nemauluma <koniecharity@gmail.com>
2024-10-14 15:32:20 +0200

    task form design

--- client/src/Pages/Task/createTask.js
+++ client/src/Pages/Task/createTask.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Sparkle = ({ style }) => (
   <motion.div
