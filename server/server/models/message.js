6351f6ecf62363058e100829047f76aa5d4feea2
Konanani Nemauluma <koniecharity@gmail.com>
2024-10-16 17:52:41 +0200

    added model for receiving messages

---
+++ server/server/models/message.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
